e7c1632331e35b1f8ae4e04d49573517beb1f924
kolesnikdi <kolesnik.d.i@gmail.com>
2022-10-04 17:37:04 +0300

    [dev] All works according to tutorial.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-09o9c*xijm-a21q5--u6m^b4!fgvvufdf2iy0dh+!dh256u*o^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['unnamed.pythonanywhere.com']
+ALLOWED_HOSTS = ['unnamed.pythonanywhere.com',
+                 '127.0.0.1']
 
 
 # Application definition
