56bf70403a646d8bb0992ea3757de3d2d7c7cd5b
Artem Khriapa <art.pzt@gmail.com>
2022-10-05 19:59:25 +0300

    Update urls.py
    example DRF

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-
+    
+    # path('json/post/', ListPostView.as_view(), name='json_post_detail'), # Get all!
+    # path('json/post/<int:pk>/', PostView.as_view(), name='json_post_detail'), # Get single!
 ]
