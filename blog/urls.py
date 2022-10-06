87e62ad14ec69517d8492886b02feae91b825b78
kolesnikdi <113462756+kolesnikdi@users.noreply.github.com>
2022-10-06 14:41:06 +0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -8,7 +8,4 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    
-    # path('json/post/', ListPostView.as_view(), name='json_post_detail'), # Get all!
-    # path('json/post/<int:pk>/', PostView.as_view(), name='json_post_detail'), # Get single!
 ]
